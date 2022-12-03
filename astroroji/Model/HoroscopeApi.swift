c77a562bd5d108ed113ed37faf83756e09e20309
retrowhale <89171572+retrowhale@users.noreply.github.com>
2022-12-03 00:04:37 -0800

    0.9 Update
    API Change, Firebase installation

--- astroroji/Model/HoroscopeApi.swift
+++ astroroji/Model/HoroscopeApi.swift
@@ -9,64 +9,73 @@ import Foundation
 import Network
 
 // MARK: - HoroscopeAPIJsonFile
-struct HoroscopeAPI: Decodable {
-    let sign: String?
-    let date: String?
-    let horoscope: String?
+struct HoroscopeAPI: Codable {
+
+    let currentDate, compatibility, luckyTime, luckyNumber: String?
+    let color, dateRange, mood, aztroDescription: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case currentDate = "current_date"
+        case compatibility
+        case luckyTime = "lucky_time"
+        case luckyNumber = "lucky_number"
+        case color
+        case dateRange = "date_range"
+        case mood
+        case aztroDescription = "description"
+    }
+    
+    
 }
 
 
 class Network: ObservableObject {
     // MARK: - To push text to view
+    @Published var currentDate : String = ""
+    @Published var aztroDescription : String = ""
+    @Published var compatibility : String = ""
+    @Published var luckyTime : String = ""
+    @Published var color : String = ""
+    @Published var dateRange : String = ""
+    @Published var mood : String = ""
     @Published var sign : String = ""
-    @Published var date : String = ""
-    @Published var horoscopeInfo : String = ""
-    @Published var horoscopeArray = []
+    @Published var luckyNumber: String = ""
     
-    // MARK: - APIURLS
-    @Published var aquariusURL = "https://ohmanda.com/api/horoscope/aquarius/"
-    @Published var picsecURL =  "https://ohmanda.com/api/horoscope/pisces/"
-    @Published var ariesURL = "https://ohmanda.com/api/horoscope/aries/"
-    @Published var taurusURL = "https://ohmanda.com/api/horoscope/taurus/"
-    @Published var geminiURL = "https://ohmanda.com/api/horoscope/gemini/"
-    @Published var cancerURL = "https://ohmanda.com/api/horoscope/cancer/"
-    @Published var leoURL = "https://ohmanda.com/api/horoscope/leo/"
-    @Published var virgoURL = "https://ohmanda.com/api/horoscope/virgo/"
-    @Published var libraURL = "https://ohmanda.com/api/horoscope/libra/"
-    @Published var scorpioURL = "https://ohmanda.com/api/horoscope/scorpio/"
-    @Published var sagittariusURL = "https://ohmanda.com/api/horoscope/sagittarius/"
-    @Published var capricornURL = "https://ohmanda.com/api/horoscope/capricorn/"
-    
+
     
     // MARK: - APICallFunction
     
- 
-    func getHoroscope(urlString: String) {
-        
-        guard let url = URL(string: urlString) else { fatalError("invalid url")}
-        let urlRequest = URLRequest(url: url)
-        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let error = error {
-                print("Request error:", error)
-                return
-            }
-            guard let response = response as? HTTPURLResponse else {return}
-            
-            if response.statusCode == 200 {
-                guard let data = data else {return}
-                DispatchQueue.main.async {
-                    do {
-                        let decodedHoroscope = try JSONDecoder().decode(HoroscopeAPI.self, from: data)
-                        self.sign = decodedHoroscope.sign!
-                        self.date = decodedHoroscope.date!
-                        self.horoscopeInfo = decodedHoroscope.horoscope!
-                        
-                    }catch let error {
-                        print("Error decoding:", error)
-                    }
-                }
-            }
-        }
-        dataTask.resume()
-    }
+    func getHoroscope(horoscope: String, date: String) {
+           
+           guard let url = URL(string: "https://aztro.sameerkumar.website/?sign=\(horoscope)&day=\(date)") else { fatalError("invalid url")}
+           var urlRequest = URLRequest(url: url)
+           urlRequest.httpMethod = "POST"
+           let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+               if let error = error {
+                   print("Request error:", error)
+                   return
+               }
+               guard let response = response as? HTTPURLResponse else {return}
+               
+               if response.statusCode == 200 {
+                   guard let data = data else {return}
+                   DispatchQueue.main.async {
+                       do {
+                           let decodedHoroscope = try JSONDecoder().decode(HoroscopeAPI.self, from: data)
+                           self.currentDate = decodedHoroscope.currentDate!
+                           self.aztroDescription = decodedHoroscope.aztroDescription!
+                           self.compatibility = decodedHoroscope.compatibility!
+                           self.luckyTime = decodedHoroscope.luckyTime!
+                           self.luckyNumber = decodedHoroscope.luckyNumber!
+                           self.color = decodedHoroscope.color!
+                           self.dateRange = decodedHoroscope.dateRange!
+                           self.mood = decodedHoroscope.mood!
+                       }catch let error {
+                           print("Error decoding:", error)
+                       }
+                   }
+               }
+           }
+           dataTask.resume()
+       }
 }
